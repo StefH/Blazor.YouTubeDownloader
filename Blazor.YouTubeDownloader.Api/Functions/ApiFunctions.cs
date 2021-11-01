1de011f9e55f53e466d41db7daed6ee7383e5f01
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-11-01 17:51:22 +0000

    .

--- Blazor.YouTubeDownloader.Api/Functions/ApiFunctions.cs
+++ Blazor.YouTubeDownloader.Api/Functions/ApiFunctions.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Extensions.Services;
 using System.Threading.Tasks;
@@ -19,12 +21,14 @@ namespace Blazor.YouTubeDownloader.Api.Functions
     {
         private readonly ILogger<ApiFunctions> _logger;
         private readonly YoutubeClient _client;
+        private readonly IHttpClientFactory _factory;
         private readonly ISerializer _serializer;
 
-        public ApiFunctions(ILogger<ApiFunctions> logger, YoutubeClient client, ISerializer serializer)
+        public ApiFunctions(ILogger<ApiFunctions> logger, YoutubeClient client, IHttpClientFactory factory, ISerializer serializer)
         {
             _logger = logger;
             _client = client;
+            _factory = factory;
             _serializer = serializer;
         }
 
@@ -71,9 +75,25 @@ namespace Blazor.YouTubeDownloader.Api.Functions
 
             var streamInfo = await _serializer.DeserializeAsync<AudioOnlyStreamInfo>(req.Body);
 
-            await using var destinationStream = new MemoryStream();
+            var httpClient = _factory.CreateClient();
 
-            await _client.Videos.Streams.CopyToAsync(streamInfo, destinationStream);
+            byte[] bytes;
+            try
+            {
+                bytes = await httpClient.GetByteArrayAsync(streamInfo.Url);
+            }
+            catch (Exception e)
+            {
+                int xxx = 0;
+                throw;
+            }
+            await using var destinationStream = new MemoryStream(bytes);
+
+            //using var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+            //await stream.CopyToAsync(destinationStream);
+
+            //await _client.Videos.Streams.CopyToAsync(streamInfo, destinationStream);
+
             destinationStream.Position = 0;
 
             var oggOpusStream = new MemoryStream();
--- Blazor.YouTubeDownloader.Api/Startup.cs
+++ Blazor.YouTubeDownloader.Api/Startup.cs
@@ -32,12 +32,13 @@ namespace Blazor.YouTubeDownloader.Api
                 .CreateLogger();
             builder.Services.AddLogging(lb => lb.AddSerilog(logger, dispose: true));
 
-            //builder.Services.AddScoped(sp =>
-            //{
-            //    var httpClient = new HttpClient(new YouTubeCookieConsentHandler());
-            //    return new YoutubeClient(httpClient);
-            //});
-            builder.Services.AddScoped<YoutubeClient>();
+            builder.Services.AddScoped(sp =>
+            {
+                var httpClient = new HttpClient(new YouTubeCookieConsentHandler());
+                return new YoutubeClient(httpClient);
+            });
+            // builder.Services.AddScoped<YoutubeClient>();
+            builder.Services.AddHttpClient();
 
             builder.Services.AddSingleton<ISerializer, Serializer>();
         }
