8c469b53deecfadb838e3e974ca60e46c15b782c
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-16 22:43:25 +0100

    2

---
+++ FunctionAppIsolated/Extensions/HttpRequestDataExtensions.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.Azure.Functions.Worker.Http;
+
+//namespace FunctionAppIsolated.Extensions
+//{
+//    internal static class HttpRequestDataExtensions
+//    {
+//        public static HttpResponseData X(this HttpRequestData req)
+//        {
+
+//        }
+//    }
+//}
---
+++ FunctionAppIsolated/Program.cs
@@ -0,0 +1,45 @@
+using System.Text.Json.Extensions.Services;
+using Blazor.YouTubeDownloader.Api.HttpClientHandlers;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using YoutubeExplode;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("local.settings.json", true, false)
+    .AddEnvironmentVariables()
+    .Build();
+
+// Register Serilog provider
+var logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.ApplicationInsights(TelemetryConfiguration.CreateDefault(), TelemetryConverter.Traces)
+    .ReadFrom.Configuration(config)
+    .CreateLogger();
+
+var builder = new HostBuilder();
+
+builder.ConfigureServices(serviceCollection =>
+{
+    serviceCollection.AddLogging(lb => lb.AddSerilog(logger, dispose: true));
+
+    serviceCollection.AddScoped(_ =>
+    {
+        var httpClient = new HttpClient(new YouTubeCookieConsentHandler());
+        return new YoutubeClient(httpClient);
+    });
+
+    serviceCollection.AddHttpClient();
+
+    serviceCollection.AddSingleton<ISerializer, Serializer>();
+});
+
+var host = builder
+    .ConfigureFunctionsWorkerDefaults()
+    .Build();
+
+host.Run();
