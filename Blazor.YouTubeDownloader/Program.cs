f0c6d223f06500cb81d15528a83e36252d25983e
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-09-08 17:32:55 +0200

    config

--- Blazor.YouTubeDownloader/Program.cs
+++ Blazor.YouTubeDownloader/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine("isLocalHost = " + isLocalHost);
 bool isAzure = baseAddress.Contains("azurestaticapps.net") || baseAddress.Contains("youtube-downloader.heyenrath.nl");
 Console.WriteLine("isAzure = " + isAzure);
 
-string httpClientBaseAddress = isLocalHost ? "http://localhost:7034/" : baseAddress;
+string httpClientBaseAddress = isLocalHost ? "http://localhost:7071/" : baseAddress;
 Console.WriteLine("httpClientBaseAddress = " + httpClientBaseAddress);
 
 builder.Services
