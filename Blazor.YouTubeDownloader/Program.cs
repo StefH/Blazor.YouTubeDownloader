822a7788e54b88af3585fb03926d9a84feccda69
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-09-08 11:33:16 +0200

    Use @azure/static-web-apps-cli for deploying

--- Blazor.YouTubeDownloader/Program.cs
+++ Blazor.YouTubeDownloader/Program.cs
@@ -1,65 +1,11 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Blazor.YouTubeDownloader.Services;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+using Blazor.YouTubeDownloader;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using RestEase;
 
-namespace Blazor.YouTubeDownloader
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            // HttpClient
-            var baseAddress = builder.HostEnvironment.BaseAddress;
-            Console.WriteLine("HostEnvironment.BaseAddress = " + baseAddress);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            bool isLocalHost = baseAddress.Contains("localhost");
-            Console.WriteLine("isLocalHost = " + isLocalHost);
-
-            bool isAzure = baseAddress.Contains("azurestaticapps.net") || baseAddress.Contains("youtube-downloader.heyenrath.nl");
-            Console.WriteLine("isAzure = " + isAzure);
-
-            string httpClientBaseAddress = isLocalHost ? "http://localhost:7034/" : baseAddress;
-            Console.WriteLine("httpClientBaseAddress = " + httpClientBaseAddress);
-
-            builder.Services
-                // Blazorise
-                .AddBlazorise(options =>
-                {
-                    options.ChangeTextOnKeyPress = true;
-                })
-                .AddBootstrapProviders()
-                .AddFontAwesomeIcons()
-
-                // BlazorDownloadFile
-                .AddBlazorDownloadFile()
-
-                // Own services
-                .AddScoped(sp =>
-                {
-                    var httpClient = new HttpClient
-                    {
-                        BaseAddress = new Uri(httpClientBaseAddress)
-                    };
-                    return new RestClient(httpClient).For<IYouTubeDownloadApi>();
-                });
-
-            var host = builder.Build();
-
-            host.Services
-                .UseBootstrapProviders()
-                .UseFontAwesomeIcons();
-
-            await host.RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
